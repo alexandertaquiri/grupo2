2c512a278e8c4cbe99ed7e5efa8a56415add8c60
Adnan <ferrer.adnan@gmail.com>
2017-06-10 18:44:50 -0300

    comprarcredito.php deleted and few changes to custom.css

---
+++ gauchada/checkCredit.php
@@ -0,0 +1,18 @@
+<?php
+	include("connectDataBase.php");
+    $con = "";
+    $usId = "";
+    $res = ""; 
+    $row = "";
+    $am = "";
+
+    function hasCredit() {
+    	global $con, $usId, $res, $row, $am;
+    	$con = connect();
+    	$usId = $_SESSION['id'];
+    	$res = mysqli_query($con,"SELECT * FROM credito WHERE idUsuario=$usId"); 
+    	$row = mysqli_fetch_array($res);
+    	$am = $row['monto'];
+    	return $am > 0;
+    }
+?>
