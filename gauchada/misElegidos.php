b508682922766da30164a0da4770839659bd4152
geressebas1987 <geressebas@gmail.com>
2017-07-03 18:06:26 -0300

    Add files via upload

--- gauchada/misElegidos.php
+++ gauchada/misElegidos.php
@@ -65,14 +65,16 @@
       							$row = mysqli_fetch_array($resx);
       							
 								$idPostulacion=$row['idPostulacion'];
-								$calificado=mysqli_fetch_array(mysqli_query($con,"SELECT elige.comentario FROM elige WHERE idPostulacion=$idPostulacion"));
+								
+								$calificado=mysqli_fetch_array(mysqli_query($con,"SELECT elige.calificacion FROM elige WHERE idPostulacion=$idPostulacion"));
 								$replica=mysqli_fetch_array(mysqli_query($con,"SELECT elige.replica FROM elige WHERE idPostulacion=$idPostulacion"));
-      							?><tr>
+      							$comentario=mysqli_fetch_array(mysqli_query($con,"SELECT elige.comentario FROM elige WHERE idPostulacion=$idPostulacion"));
+								?><tr>
 									<td><?php echo "$row[titulo]"?></td>
 									<td><?php echo "$row[nombre]"?></td>
 								<?php
-								//si hay comentario
-								if($calificado[0]==NULL){
+								//Si no tiene calificacion, comentario, ni replica
+								if($calificado[0]==NULL && $comentario[0]==NULL && $replica[0]==NULL){
 								?>
 									<td>
 									
@@ -81,30 +83,62 @@
 										<button type="button" class=b3 onClick="location.href='comentarCalificacion.php?id=<?php echo $idUser; ?>&puntaje=<?php echo $negativo; ?>&idPostulacion=<?php echo $idPostulacion; ?>&calificacion=<?php echo "negativo" ?>' " > <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> </button>
 								
 									</td>			
-								<?php
-								}else if($calificado[0]!=NULL && $replica[0]==NULL){
-										//si hay comentario, pero falta la replica
-									?>
-										<td><?php echo "$row[calificacion]"?></td>
-										<td><?php echo "$row[comentario]"?></td>
-										<td><button type="button" class="btn btn-info" onClick="location.href='replicarCalificacion.php?idPostulacion=<?php echo $idPostulacion; ?>' " > Responder </button></td>
-										
-									<?php
-								}else{
-      							?>
-								<td><?php echo "$row[calificacion]"?></td>
-								<td><?php echo "$row[comentario]"?></td>
-								<td><?php echo "$row[replica]"?></td>
-      							<?php
-								}
-								?>
-								<td> <?php echo"<img src=";
+									<td></td>
+									<td></td>
+									
+									<td> 
+									<?php echo"<img src=";
       								if(($row['foto'])==""){
       									echo"./imgs/logo2.jpg";
       								}else echo"mostrarImagen2.php?idUsuario=".$row['idUsuario'];
       								echo">"
 									?></td>
                                     </tr>
+									
+								<?php
+									//si tiene calificacion, comentario pero no replica, y el usuario actual puede contestar
+									
+								}else if($calificado[0]!=NULL && $comentario[0]!=NULL && $replica[0]==NULL && $idUser==$row['idUsuario']){
+										//si hay comentario, pero falta la replica
+									?>	
+										
+										<td><?php echo "$row[calificacion]"?></td>
+										<td><?php echo "$row[comentario]"?></td>
+										<td><button type="button" class="btn btn-info" onClick="location.href='replicarCalificacion.php?idPostulacion=<?php echo $idPostulacion; ?>' " > Responder </button></td>
+										
+											<td> 
+										<?php echo"<img src=";
+										if(($row['foto'])==""){
+											echo"./imgs/logo2.jpg";
+										}else echo"mostrarImagen2.php?idUsuario=".$row['idUsuario'];
+										echo">"
+										?></td>
+										</tr>
+										
+									<?php
+								
+									
+									//si tiene calificacion, comentario y replica
+								} else{
+										?>
+										<td><?php echo "$row[calificacion]"?></td>
+										<td><?php echo "$row[comentario]"?></td>
+										<td><?php echo "$row[replica]"?></td>
+										<td> 
+										<?php echo"<img src=";
+										if(($row['foto'])==""){
+											echo"./imgs/logo2.jpg";
+										}else echo"mostrarImagen2.php?idUsuario=".$row['idUsuario'];
+										echo">"
+										?></td>
+										</tr>
+										
+										<?php
+								}
+								?>
+									
+									
+									
     						  <?php
 							  }
       					}
